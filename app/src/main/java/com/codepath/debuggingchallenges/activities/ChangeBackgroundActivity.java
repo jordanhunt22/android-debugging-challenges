b7c955322422f8502fa97e939969189a60f45a49
Jordan Hunt <jordanhunt@fb.com>
2020-06-25 10:19:35 -0700

    "Background changer and toolbar debugged"

--- app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
@@ -4,18 +4,23 @@ import android.graphics.Color;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.codepath.debuggingchallenges.R;
 
 public class ChangeBackgroundActivity extends AppCompatActivity {
 
     private int oldColor = Color.BLUE;
+    Button btnGo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_background);
+        btnGo = (Button) findViewById(R.id.btnGo);
     }
+
 
     public void onGoClick(View view) {
         View mainView = findViewById(android.R.id.content);
